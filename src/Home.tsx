ca1c30149f2b945e34503fe1504b6a00e070d3ea
Raye <9711442+rayeschiller@users.noreply.github.com>
2022-06-29 15:50:18 -0700

    Uterverse nft storefront

--- src/Home.tsx
+++ src/Home.tsx
@@ -650,12 +650,12 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>My NFT</h2>
+                            <h2>Uterverse NFT Mint</h2>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
-                                src="cool-cats.gif"
-                                alt="NFT To Mint"/></div>
+                                src="uterverse.gif"
+                                alt="Uterverse NFT To Mint"/></div>
                             <br/>
                             {wallet && isActive && whitelistEnabled && (whitelistTokenBalance > 0) && isBurnToken &&
                               <h3>You own {whitelistTokenBalance} WL
